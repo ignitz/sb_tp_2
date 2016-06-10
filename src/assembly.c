dc959907fa45db1347105f4ae4d8c05b511af463
ignitz <ignitzhjfk@gmail.com>
2016-06-10 16:52:01 -0300

    Corrigido escrita
    Está errado a posição de memória (que achei que era absoluta)
    E interpretei que memória era imediato (Está muito confuso pra diferenciar um ao outro)

--- src/assembly.c
+++ src/assembly.c
@@ -35,6 +35,7 @@ void pass_one(FILE *entrada) {
 				location_counter += get_size(token);
 			} else { // Operandos
 				
+				// TODO Implementar variável na memória
 				#ifdef DEBUG
 					printf(" .%s ", token);
 				#endif
@@ -59,7 +60,6 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 	int buffer,
 		operando1, operando2;
 
-	int flagOperando; // tipos de Operando
 	// 0  Nenhum operando 
 	// 1  Registrador 
 	// 2  Memória 
@@ -69,6 +69,7 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 	// 6  Memória e imediato (número)
 	// 7  Registrador e imediato (número) 
 	// 8  Imediato (número) 
+	int flagOperando; // tipos de Operando
 
 	// Insere a posição inicial do programa
 	fwrite(&posicaoInicial, 2, 1, objeto);
@@ -83,13 +84,15 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 				// Ignora label
 			} else if (get_opcode(token) > 0) {
 				// Aqui faço a análise da linha
+
 				switch(get_size(token)) {
 					case 2: // 16 bits
+
 						buffer = get_opcode(token) << 8;
 						flagOperando = 0;
 						break;
 					case 4: // 32 bits
-						
+				
 						buffer = get_opcode(token) << 8;
 						token = strtok(NULL, CHAR_IGNORE);
 
@@ -132,17 +135,85 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 						buffer += flagOperando;
 						break;
 					case 6: // 48 bits
+
 						buffer = get_opcode(token) << 8;
-						token = strtok(NULL, CHAR_IGNORE);
-
-						
 
 						token = strtok(NULL, CHAR_IGNORE);
+
+						// Falta implementar acesso a memória por variável
+						if (strcmp(token, "AL") == 0) {
+							operando1 = 0;
+							flagOperando = 1;
+						} else if (strcmp(token, "AH") == 0) {
+							operando1 = 1;
+							flagOperando = 1;
+						} else if (strcmp(token, "AX") == 0) {
+							operando1 = 2;
+							flagOperando = 1;
+						} else if (strcmp(token, "BH") == 0) {
+							operando1 = 3;
+							flagOperando = 1;
+						} else if (strcmp(token, "BL") == 0) {
+							operando1 = 4;
+							flagOperando = 1;
+						} else if (strcmp(token, "BX") == 0) {
+							operando1 = 5;
+							flagOperando = 1;
+						} else if (strcmp(token, "CL") == 0) {
+							operando1 = 6;
+							flagOperando = 1;
+						} else if (strcmp(token, "CH") == 0) {
+							operando1 = 7;
+							flagOperando = 1;
+						} else if (strcmp(token, "CX") == 0) {
+							operando1 = 8;
+							flagOperando = 1;
+						}
+
+						token = strtok(NULL, CHAR_IGNORE);
+
 						// 3  Registrador e memória 
 						// 4  Memória e Registrador 
 						// 5  Registrador e Registrador 
 						// 6  Memória e imediato (número)
 						// 7  Registrador e imediato (número) 
+
+						// Falta implementar acesso a memória por variável
+						if (strcmp(token, "AL") == 0) {
+							operando2 = 0;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "AH") == 0) {
+							operando2 = 1;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "AX") == 0) {
+							operando2 = 2;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "BH") == 0) {
+							operando2 = 3;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "BL") == 0) {
+							operando2 = 4;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "BX") == 0) {
+							operando2 = 5;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "CL") == 0) {
+							operando2 = 6;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "CH") == 0) {
+							operando2 = 7;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (strcmp(token, "CX") == 0) {
+							operando2 = 8;
+							if (flagOperando == 1) flagOperando = 5; else if (flagOperando == 2) flagOperando = 4; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (token[0] == '_') {
+							operando2 = get_symbol_address(token);
+							if (flagOperando == 1) flagOperando = 7; else if (flagOperando == 2) flagOperando = 6; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						} else if (token[0] == '0' && token[1] == 'x') {
+							operando2 = get_hex_value(token);
+							if (flagOperando == 1) flagOperando = 7; else if (flagOperando == 2) flagOperando = 6; else {printf("Erro no tipo de operandos\n"); exit(0);}
+						}
+
 						buffer += flagOperando;
 						break;
 					default:
@@ -154,8 +225,8 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 						break;
 				}
 
+				// Passo da escrita no arquivo
 				switch (flagOperando) {
-
 					case 0: // 0  Nenhum operando
 						fwrite(&buffer, 2, 1, objeto);
 						break;
@@ -179,12 +250,8 @@ void pass_two(FILE *entrada, FILE *objeto, int posicaoInicial) {
 
 					default: // Se der algum problema
 						printf("Erro em retorno de tipo de operando\n");
-						fclose(objeto);
-						fclose(entrada);
-						exit(0);
 						break;
 				}
-				fwrite(&buffer, 2, 1, objeto);
 
 			} else { // Caso ocorra algo inexpedado
 				printf("Exceção:\n");
